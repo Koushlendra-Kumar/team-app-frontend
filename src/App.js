7b7b8d89a70b13fb3aab95f0ee2ec111010932f2
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-11-24 09:47:31 +0000

    🚧 Create root files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Card from "./Components/Card/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Card first_name="John" last_name="Doe" email="XXXXXXXXXXXXXXXXX" gender="Male" avatar="https://robohash.org/sintessequaerat.png?size=50x50&set=set1" domain="Sales" available={true}/>
+    </div>
+  );
+}
+
+export default App;
